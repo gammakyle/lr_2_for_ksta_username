35864c45f7d0de41f88c501bd3049ea873206b25
username <youremail>
2023-10-03 19:04:54 +0300

    rebuild for only second values

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@ int main() {
    int N;
    cout << "Введите количество итераций N: ";
    cin >> N;
-
+   int ticker = 0;
    int a = 0, b = 1, nextTerm;
    cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
 
-   for (int i = 1; i <= N; ++i) {
-      cout << a << " ";
+   for (int i = 1; i <= N; i++) {
+        if(ticker == 2)
+        {
+            cout << a << " ";
+            ticker = 0;
+        }
       nextTerm = a + b;
       a = b;
       b = nextTerm;
+      ticker++;
    }
    return 0;
 }
